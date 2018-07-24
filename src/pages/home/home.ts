1450c7f8d0819c1370c57fa721cba745318d0ab3
Mauricio <mgaldamesc.azurian@bancochile.cl>
2018-07-24 16:44:22 -0500

    integration with openweathermap api to get weather info of location inserted by user input

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import { Http } from '@angular/http';
 
 @Component({
@@ -8,23 +8,32 @@ import { Http } from '@angular/http';
 })
 export class HomePage {
 
-  users: any[];
+  weatherInfo: any = {};
+  cityName: string = "Santiago";
+  apiKey: string = "ENTER_API_KEY_HERE";
+  mapLocation: string = "";
 
   constructor(public navCtrl: NavController
-    , public http: Http) {
+    , public http: Http
+    , public loadingController: LoadingController) {
 
       this.getData();
 
   }
 
   getData() {
+    let loading = this.loadingController.create();
+    loading.present();
     // subscribe function register the handler or callback functions which will be executed
     // when http request either succeeds or fails
-    this.http.get("http://jsonplaceholder.typicode.com/users").subscribe((data) => {
+    this.http.get("http://api.openweathermap.org/data/2.5/weather?q=" + this.cityName + "&appid=" + this.apiKey).subscribe((data) => {
       console.log(data.json());
-      this.users = data.json();
+      this.weatherInfo = data.json();
+      this.mapLocation = "https://maps.googleapis.com/maps/api/staticmap?center="+this.cityName+"&size=600x300";
+      loading.dismiss();
     }, (error) => {
       console.log(error);
+      loading.dismiss();
     });
   }
 
